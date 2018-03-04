45e43911b325626ece537187a1942e76482d4c36
Modou Lo <modoulo99@icloud.com>
2018-03-04 08:55:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,4 +8,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
  return playlist
 }
 
-function 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
